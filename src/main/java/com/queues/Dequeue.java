6a71b9eed14983848b274855eed1e60e7a0f1fe7
prokopenya <igorprokopenya@gmail.com>
2020-01-18 18:06:16 +0300

    simple Dequeu

---
+++ src/main/java/com/queues/Dequeue.java
@@ -0,0 +1,86 @@
+package com.queues;
+
+import java.util.NoSuchElementException;
+
+public class Dequeue<Item> {
+
+    private Node first, last;
+    private int size = 0;
+
+    public Dequeue() {
+        first = null;
+        last = null;
+    }
+
+    public void addFirst(Item item){
+        if (item == null)
+            throw new NullPointerException("null values cannot be inserted.");
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        first.prev = null;
+        if (last == null)
+            last = first;
+        else
+            oldFirst.prev = first;
+        size++;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException("no element exists on deque.");
+
+        Item item = first.item;
+        if (first == last)
+            first = last = null;
+        else {
+            first = first.next;
+            first.prev = null;
+        }
+        size--;
+        return item;
+    }
+    public void addLast(Item item){
+        if (item == null)
+            throw new NullPointerException("null values cannot be inserted.");
+        if(isEmpty())
+            addFirst(item);
+        else{
+            Node oldLast = last;
+            last = new Node();
+            last.item = item;
+            last.next = null;
+            last.prev = oldLast;
+            oldLast.next = last;
+        }
+    }
+
+    public Item removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException("no element exists on deque.");
+        Item item = last.item;
+        if (first == last)
+            first = last = null;
+        else {
+            last = last.prev;
+            last.next = null;
+        }
+        return item;
+    }
+
+
+    public boolean isEmpty() {
+        return first == null && last == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+    }
+}
